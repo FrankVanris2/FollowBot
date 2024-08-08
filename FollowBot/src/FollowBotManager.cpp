458ba6ca7a78f0b1ac743dd23c4429dc8a82e248
FrankVanris2 <frankcvanris@gmail.com>
2024-08-08 12:49:24 -0700

    Included the necessary files for Arduino
    These files will be needed in order to run our robot properly. Used Platform IO in order to write to the arduino properly. Created a new project FollowBot, currently has the necessary setup for us to run the project properly. I have the main, and the managers.

---
+++ FollowBot/src/FollowBotManager.cpp
@@ -0,0 +1,21 @@
+/*
+* By: Frank Vanris
+* Date: 8/8/2024
+* Desc: Creating a manager of all the methods and objects that will run in the main
+* via the setup and loop
+*/
+
+#include "FollowBotManager.h"
+
+//universal object
+FollowBotManager followBotManager;
+
+FollowBotManager::FollowBotManager() {}
+
+
+//the setup that will store the many objects that will set in the main
+void FollowBotManager::followBotSetup() {}
+
+
+//the loop that will store the many objects that will loop in the main
+void FollowBotManager::followBotLoop() {}
