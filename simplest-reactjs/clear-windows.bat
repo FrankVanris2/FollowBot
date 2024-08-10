f4f4ce0c26614370afaae2eb946fef14cc2b1ab3
FrankVanris2 <frankcvanris@gmail.com>
2024-08-09 19:43:18 -0700

    added server with react/python
    added the server with react/python that will be used to interact with the robot.

---
+++ simplest-reactjs/clear-windows.bat
@@ -0,0 +1,2 @@
+rmdir/q/s dist node_modules
+del package-lock.json
---
+++ simplest-reactjs/src/App.style.js
@@ -0,0 +1,22 @@
+import styled, {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Arial;
+    font-size: 14px;
+    overflow: auto;
+  }
+`;
+
+// Example style (remove it when writing your own code)
+export const HelloWorld = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 150px;
+  margin: 20px;
+  color: lightBlue;
+  font-weight: bolder;
+  background: linear-gradient(0.1turn, red, green);
+`;
