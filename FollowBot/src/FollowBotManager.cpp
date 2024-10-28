773d51014dbf3715db3696c7e41ca4de13f1da8c
FrankCVanris <frankcvanris@gmail.com>
2024-10-28 14:51:47 -0700

    Minor changes for publisher information
    this is ros serial for testing purposes but is in pause till Raspberry Pi 4B arrives.

--- FollowBot/src/FollowBotManager.cpp
+++ FollowBot/src/FollowBotManager.cpp
@@ -12,8 +12,21 @@
 #include "TemperatureReader.h"
 #include "FollowBotClient.h"
 #include "FollowMechanics.h"
+
+//testing currently
+#include <ros.h>
+#include <std_msgs/String.h>
+
 //universal object
 FollowBotManager followBotManager;
+
+// Ros testing code
+ros::NodeHandle nh;
+
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+
+char hello[13] = "hello world!";
 
 FollowBotManager::FollowBotManager(): mIsDirty(false) {
     //mDirection(MOTOR_STOP)
@@ -22,31 +35,40 @@ FollowBotManager::FollowBotManager(): mIsDirty(false) {
 
 //the setup that will store the many objects that will set in the main
 void FollowBotManager::followBotSetup() {
-    myMotors.motorSetup();
+
+    //Testing Ros Setup code
+    nh.initNode();
+    nh.advertise(chatter);
+
+    /*myMotors.motorSetup();
     followBotClient.followBotClient_Setup();
     temperatureReader.temperatureReader_Setup();
     objectAvoidance.objectAvoidance_Setup();
-    Serial.println("Finished Setup");
+    Serial.println("Finished Setup");*/
+
 }
 
 //the loop that will store the many objects that will loop in the main
 void FollowBotManager::followBotLoop() {
-    followBotClient.followBotClient_Loop();
-    
 
+    // Testing Ros loop code
+    str_msg.data = hello;
+    chatter.publish( &str_msg );
+    nh.spinOnce();
+    delay(1000);
+
+    /*
+    followBotClient.followBotClient_Loop();
     //Avoiding Obstacles
     objectAvoidance.objectAvoidance_Loop();
-    
     //if(objectAvoidance.getDistance1() <= 20 || objectAvoidance.getDistance2() <= 20) {
         //myMotors.motorStop();
     //}
-    
     //obtaining the Temperature
     temperatureReader.temperatureReader_Loop();
-
     followMechanics.followMechanics_Loop();
-
     // For motor movement
     myMotors.motorLoop();
+    */
  
 }
