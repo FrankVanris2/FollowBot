eb8fe451622120694f4b02844cc7c2b5eba97645
FrankCVanris <frankcvanris@gmail.com>
2025-03-05 17:57:06 -0800

    added two files
    These 2 files will be used for gps stuff

---
+++ FollowBot/src/gps/GPS.cpp
@@ -0,0 +1,5 @@
+/*
+By: Frank Vanris
+Date: 3/5/2025
+Desc: Creating a gps class which allows me to send position data to the raspberry pi in order to let the user know where the robot is.
+*/
---
+++ FollowBot/src/gps/GPS.h
@@ -0,0 +1,20 @@
+/*
+By: Frank Vanris
+Date: 3/5/2025
+Desc: Creating a GPS module that will be used in order to obtain the actual position of wheret the robot is.
+*/
+
+#pragma once
+
+class GPS {
+
+public:
+
+    GPS();
+
+    void gps_setup();
+    void gps_loop();
+
+private:
+
+}
