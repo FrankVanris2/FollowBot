9939039fcd8545573517aa97fda1eb53b59bc5ce
FrankCVanris <frankcvanris@gmail.com>
2025-03-08 19:16:35 -0800

    fixed
    added a minor fix

--- FollowBot/FrontEndInterface/frontendInterface.py
+++ FollowBot/FrontEndInterface/frontendInterface.py
@@ -1,4 +1,29 @@
-'''frontend_package = {
-    "temp": 
-}'''
+import json
+
+#defining a schema
+FollowBotData_Schema = {
+    "type": "object",
+    "properties": {
+        "temperature": {"type": "number"},
+        #"Battery": {"type": "number"},
+        "time": {"type": "string", "format": "date-time"},
+        "coordinates": 
+            {
+                "long": {"type": "number"},
+            "lat": {"type": "number"},
+            },
+    },
+    "required": ["temperature", "battery", "time", "coordinates"]
+}
+
+#defining a example object
+FollowBotData_Example = {
+    "temperature": 25.5,
+    #"battery": 75,
+    "Time": "2023-10-01T12:00:00Z",
+    "coordinates": {
+        "long": 12.34,
+        "lat": 56.78
+    }
+}
 
--- FollowBot/src/followbot_manager/FollowBotManager.h
+++ FollowBot/src/followbot_manager/FollowBotManager.h
@@ -20,26 +20,38 @@ public:
     void followBotLoop();
     
     //temperature getters and setters
-    float getTemperature() const {
+    double getTemperature() const {
         return mOutputData.mTemperature;
     }
 
-    void setTemperatureParams(float temp, float heatIndex) {
+    void setTemperatureParams(double temp) {
         Serial.println("FollowBotManager.setTemperatureParams()");
         mIsDirty = true;
         mOutputData.mTemperature = temp;
-        mOutputData.mHeatIndex = heatIndex;
     }
 
-    //heat index getters and setters
-    float getHeatIndex() const {
-        return mOutputData.mHeatIndex;
+    //for clock
+    void setClock(int day, int month, int year, int hour, int minute, int second) {
+        Serial.println("FollowBotManager.setClcokParams()");
+
+        //Format the date and time into a single string
+        char clockBuffer[25];
+        snprintf(clockBuffer, sizeof(clockBuffer), "%02d/%02d/%04d %02d:%02d:%02d", day, month, year, hour, minute, second);
+
+        mIsDirty = true;
+        mOutputData.mClock = String(clockBuffer);
     }
+
+
+    String getClock() const { return mOutputData.mClock; }
+
+
 
     const OutputData& getOutputData() const {
         mIsDirty = false;
         return mOutputData;
     }
+
 
     bool getDirtyFlag() const {
         return mIsDirty;
