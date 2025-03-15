039266a4544b163c74c8e99af1761ed8aee0fdd5
FrankCVanris <frankcvanris@gmail.com>
2025-03-15 13:26:14 -0700

    updated the type of Node.js we will be using
    No more compilation errors or usages of old packages or versions.

--- BotServerNative/App.tsx
+++ BotServerNative/App.tsx
@@ -1,29 +1,53 @@
 import React, { useEffect } from 'react'; // Import useEffect
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { Row, Col } from 'react-native-responsive-grid-system';
 import BleManager from 'react-native-ble-manager';
 import { styles } from './App.styles';
 
-const App = () => {
-  useEffect(() => {
-    // Initialize BleManager when the component mounts
-    BleManager.start({ showAlert: false }).then(() => {
-      console.log('BleManager initialized');
-      // You can now use BleManager methods here or in other parts of your component
-    }).catch((error) => {
-      console.error('BleManager initialization error:', error);
-    });
+const startBluetooth = async () => {
+  try {
+    await BleManager.start({ showAlert: false });
+    console.log('Bluetooth started');
+  } catch (error) {
+    console.error('Bluetooth start error:', error);
+  }
+}
 
-    // Clean up when the component unmounts (optional)
+const scanBluetooth = async (isScanning : boolean) => {
+  try {
+    if (isScanning) {
+      await BleManager.scan([], 15, false);
+      const peripherals = await BleManager.getDiscoveredPeripherals();
+      console.log('Discovered peripherals:', peripherals);
+    } else {
+      await BleManager.stopScan();
+    }
+    
+  } catch (error) {
+    console.error('Scan error:', error);
+  }
+}
+
+const App = () => {
+  const [isScanning, setIsScanning] = React.useState(false);
+
+  useEffect(() => {
+    startBluetooth();
+
     return () => {
       BleManager.stopScan();
     };
   }, []);
 
+  useEffect(() => {
+    scanBluetooth(isScanning);
+  }, [isScanning]);
+
   return (
     <Row>
       <Col xs={12} sm={4} md={4} lg={4}>
-        <View><Text style={styles.header}>Column 1</Text></View>
+        <Button title='Scan' onPress={() => setIsScanning(!isScanning)} />
+        <View><Text style={styles.header}>{isScanning ? 'Scanning' : ''}</Text></View>
       </Col>
       <Col xs={12} sm={4} md={4} lg={4}>
         <View><Text style={styles.header}>Column 2</Text></View>
