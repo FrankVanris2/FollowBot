25b7fe09f0b176508b8e62c6742b860b00fe00f1
FrankCVanris <frankcvanris@gmail.com>
2025-04-12 18:54:23 -0700

    Encoder Code finalized
    The code for obtaining the ticks of the encoders have been finalized. It will be sent as a Json file to the pi as the rest of the data. Final change and pr is next.

--- FollowBot/src/followbot_manager/FollowBotManager.cpp
+++ FollowBot/src/followbot_manager/FollowBotManager.cpp
@@ -19,7 +19,6 @@
 #include "motors/Motors.h"
 
 #include "sensors/Gyroscope.h"
-#include "sensors/Compass.h"
 #include "sensors/Encoders.h"
 
 #include "gps/GPS.h"
@@ -40,66 +39,40 @@ FollowBotManager::FollowBotManager(): mIsDirty(false), mCurrentControl(ROBOT){
     //mDirection(MOTOR_STOP)
 }
 
-
-
 //the setup that will store the many objects that will set in the main
-
 void FollowBotManager::followBotSetup() { 
-    //Following Mechanics setup
-    // myMotors.motorSetup();
-    // compass.compass_Setup();
-    // myGPS.gps_setup();
-    //followBotBluetooth.setup();
 
-    //Server Setup
-    // eepromStorage.setup();  
-    // myLCDScreen.myLCDScreen_Setup();
-    // myMotors.motorSetup();
-    // followBotClient.followBotClient_Setup();  
-    // myGPS.gps_setup();
+    eepromStorage.setup();  
+    myLCDScreen.myLCDScreen_Setup();
+    myMotors.motorSetup();
+    followBotClient.followBotClient_Setup();  
+    myGPS.gps_setup();
 
     //Testing
     encoders.setupEncoders();
-
-    
-    //ROS2 specific:
-    //gyroscope.gyroscope_Setup();
-
-    //DELAYED
-    // temperatureReader.temperatureReader_Setup();
-    // objectAvoidance.objectAvoidance_Setup();
-    // objectDetection.objectDetection_Setup();
-    
-    
+  
+    // ROS2 specific:
+    gyroscope.gyroscope_Setup();
 
 }
 
 void FollowBotManager::followBotLoop() {
-    //Following Mechanics loop setup
-    // myGPS.gps_loop();
-    // followMechanics.followMechanics_Loop();
-    // compass.compass_loop();
-    //followBotBluetooth.loop();
-
-    //Server Setup
-    // myLCDScreen.myLCDScreen_Loop();
-    // followBotClient.followBotClient_Loop(); 
-    // myGPS.gps_loop(); 
-    // if (mCurrentControl == ROBOT) {
-    //     followMechanics.followMechanics_Loop();
-    // }
-    // myMotors.motorLoop();
+  
+    myLCDScreen.myLCDScreen_Loop();
+    followBotClient.followBotClient_Loop(); 
+    myGPS.gps_loop(); 
+    if (mCurrentControl == ROBOT) {
+        followMechanics.followMechanics_Loop();
+    }
+    myMotors.motorLoop();
 
     // Testing
     encoders.loopEncoders();
 
     //ROS2 Specific
-    //gyroscope.gyroscope_Loop();
-    //ros2_serial.ros2_loop();
+    gyroscope.gyroscope_Loop();
+    ros2_serial.ros2_loop();
     
-    //DELAYED
-    //temperatureReader.temperatureReader_Loop();
-    //objectDetection.objectDetection_Loop();
 }
 
 
--- FollowBot/src/sensors/Encoders.cpp
+++ FollowBot/src/sensors/Encoders.cpp
@@ -19,13 +19,8 @@ Encoders encoders;
 volatile long Encoders::mPosition1 = 0;
 volatile long Encoders::mPosition2 = 0;
 
-
-Encoders::Encoders() : 
-kp(1), kd(0), ki(0),
-target(0), prevT(0),
-eprev1(0), eprev2(2),
-eintegral1(0.0), eintegral2(0.0) {}
-
+Encoders::Encoders() : currentTime(0), previousTime(0),
+previousPosition1(0), previousPosition2(0) {}
 
 void Encoders::setupEncoders() {
     pinMode(ENCODER_OUT_A, INPUT);
@@ -38,50 +33,23 @@ void Encoders::setupEncoders() {
 }
 
 void Encoders::loopEncoders() {
-    Serial.println(String("Encoder 1 Position: ") + mPosition1);
-    Serial.println(String("Encoder 2 Position: ") + mPosition2);
 
     // time difference
-    long currT = micros();
-    float deltaT = (currT - prevT) / 1.0e6;
-    prevT = currT;
-
-    // PID calculations for both motors
-    float u1 = calculatePID(mPosition1, eprev1, eintegral1, deltaT);
-    float u2 = calculatePID(mPosition2, eprev2, eintegral2, deltaT);
-
-    // Motor power and direction (example for motor 1, same for motor 2)
-    float pwr1 = fabs(u1) > 255 ? 255 : fabs(u1); 
-    int dir1 = u1 < 0 ? -1 : 1; // Direction based on sign of u1
-
-    float pwr2 = fabs(u2) > 255 ? 255 : fabs(u2);
-    int dir2 = u2 < 0 ? -1 : 1; // Direction based on sign of u2
-
+    currentTime = micros();
     
-    // Send signals to motors (implement motor-specific code here)
-    // Example: setMotorPower(dir1, pwr1);
-    // Example: setMotorPower(dir2, pwr2);
-
    // Debug information
-   Serial.println(String("Motor 1 Control Signal: ") + u1);
-   Serial.println(String("Motor 2 Control Signal: ") + u2);
+   Serial.println(String("Encoder 1 num ticks: ") + mPosition1);
+   Serial.println(String("Encoder 2 num ticks: ") + mPosition2);
 
-}
+    if(currentTime % THREE_SECONDS == 0) {
+        setEncoderData(mPosition1, mPosition2);
 
-// Function to calculate PID output for each motor
-float Encoders::calculatePID(long position, int &prevError, float &integral, float deltaT) {
-    int error = position - target; // Calculate error
-    float derivative = (error - prevError) / deltaT; // Calculate derivative
-    integral += error * deltaT; // Calculate integral;
-    prevError = error;
-    return kp * error + kd * derivative + ki * integral; // Calculate PID output
-}
+        previousPosition1 = mPosition1;
+        previousPosition2 = mPosition2;
+        mPosition1 = 0;
+        mPosition2 = 0;
+    }
 
-float Encoders::convertTicksToMM(int numTicks) {
-    // COnvert ticks to mm based on wheel circumference and encoder CPR
-    float numRotations = float (numTicks / ENCODER_CPR);
-    float distance = numRotations * WHEEL_CIRCUMFERENCE;
-    return distance; // in mm
 }
 
 void Encoders::readEncoder1() {
