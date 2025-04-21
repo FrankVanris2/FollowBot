8ec2ebc2714f2b38f25e24b21aaf56edc0c5302b
IgorJantGit <igorj1@live.com>
2025-04-20 22:38:47 -0700

    debugging the jest 2

--- BotServer/jest.browser.config.js
+++ BotServer/jest.browser.config.js
@@ -1,5 +1,3 @@
-import Navbar from '../components/NavBar'; // Correct path
-
 module.exports = {
   testEnvironment: 'jsdom', // Use jsdom for browser-like testing
   testMatch: ['**/__tests__/**/*.browser.test.js'], // Match browser-specific test files
