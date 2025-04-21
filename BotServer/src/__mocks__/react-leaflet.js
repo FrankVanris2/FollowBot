0a9e612d0bcc29bc60b4682e3b1889843d2f2d97
IgorJantGit <igorj1@live.com>
2025-04-21 16:35:40 -0700

    debugging the  The test browerst to jet jest to work

---
+++ BotServer/src/__mocks__/react-leaflet.js
@@ -0,0 +1,9 @@
+// filepath: c:\Users\Igorj\dev\GitHub\FollowBot\BotServer\__mocks__\react-leaflet.js
+module.exports = {
+  MapContainer: () => 'MapContainer',
+  TileLayer: () => 'TileLayer',
+  Marker: () => 'Marker',
+  Popup: () => 'Popup',
+  GeoJSON: () => 'GeoJSON',
+  useMap: jest.fn(),
+};
